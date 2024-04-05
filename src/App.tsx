396e30e3e1ff0c0b42720e6bcc58d590bbde76c8
Hubert Dumas <huberd@gmail.com>
2024-04-05 12:56:24 +1100

    Federated Sign In with Amplify

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,55 @@
-import { FC, useState } from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import { Authenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { type AuthUser } from "aws-amplify/auth";
-import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
+import viteLogo from '/vite.svg';
+import { signInWithRedirect } from 'aws-amplify/auth';
 
-
-type AppProps = {
-  signOut?: UseAuthenticator["signOut"]; //() => void;
-  user?: AuthUser;
-};
-
-const App: FC<AppProps> = ({ signOut, user }) => {
-
+const App = () => {
   return (
-    <div className="App bg-white">
-      <header className="App-header">
-        <img src={viteLogo} className="App-logo" alt="vite logo" />
+    <>
+    <Authenticator>
+      {({ signOut, user  }) => (
+        <div className="App bg-white">
+          <header>
+            <img src={viteLogo} alt="Vite Logo" className="App-logo" />
+            <Heading level={1}>
+              <div>{user?.username}</div>
+              <div>{user?.signInDetails?.loginId}</div>
+            </Heading>
+          </header>
+          <Button onClick={signOut}>Sign Out</Button>
+        </div>
 
-        <Heading level={1}>Hello {user?.username}</Heading>
-        <Button onClick={signOut}>Sign out</Button>
-        <h2>Amplify Todos</h2>
-        <p>
-          User: {user ? JSON.stringify(user, null, 2) : "None"}
-        </p>
-      </header>
-    </div>
-  )
+      )}
+    </Authenticator>
+    <FederatedSignIn />
+    
+    </>
+  );
 }
 
-export default withAuthenticator(App)
+const FederatedSignIn = () => {
+  const handleEntraSignIn = async () => {
+    try {
+      const user = await signInWithRedirect({ provider: { custom: 'azuregranadaamplify' } });
+      console.log('User signed in with Entra ID:', user);
+      // Handle successful sign-in (e.g., navigate to a different page)
+    } catch (error) {
+      console.error('Error signing in with Entra ID:', error);
+      // Handle sign-in errors
+    }
+  };
+
+  return (
+    <button onClick={handleEntraSignIn}>Sign in with Entra ID</button>
+  );
+};
+
+
+export default App;
+
+// Hosted UI URL: https://zincstoreglobal.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize?client_id=29hvg7h628crq4jnaque137cp5&response_type=token&scope=email+openid&redirect_uri=https%3A%2F%2Flocalhost%3A4310
+
+// Fed Sign In Button Link: https://zincstoreglobal.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize?identity_provider=azureadgranada&redirect_uri=https://localhost:4310&response_type=TOKEN&client_id=29hvg7h628crq4jnaque137cp5&scope=email%20openid
+// Fed Sign In Button Link:https://hdamplify.auth.ap-southeast-2.amazoncognito.com/oauth2/authorize?redirect_uri=http%3A%2F%2Flocalhost%3A4310%2F&response_type=code&client_id=56iv34do8ifj9111bdikdtlsia&identity_provider=azuregranadaamplify&scope=openid%20email%20phone%20profile%20aws.cognito.signin.user.admin%20&state=E4C6EMVUSVFeN08XoHFLcX9HnW2HnDOe&code_challenge=ARpjrY-Dih9Ml5_kI0RFV-cFPmYUFiozRQF939FfoBU&code_challenge_method=S256
+// '/oauth2/authorize?identity_provider=azuregranadaamplify&redirect_uri=https://localhost:4310&response_type=CODE&client_id=56iv34do8ifj9111bdikdtlsia&scope=email openid profile'
--- src/main.tsx
+++ src/main.tsx
@@ -12,18 +12,22 @@ Amplify.configure({
       loginWith: { // Optional
         oauth: {
           domain: 'hdamplify.auth.ap-southeast-2.amazoncognito.com',
-          scopes: ['openid email phone profile aws.cognito.signin.user.admin '],
-          redirectSignIn: ['http://localhost:4310/','https://example.com/'],
-          redirectSignOut: ['http://localhost:4310/','https://example.com/'],
+          scopes: ['openid email profile aws.cognito.signin.user.admin'],
+          redirectSignIn: ['https://localhost:4310','https://example.com/'],
+          redirectSignOut: ['https://localhost:4310','https://example.com/'],
           responseType: 'code',
+          providers: ['Google','Facebook','Amazon','Apple', {
+            custom: 'azuregranadaamplify'
+          }],
         },
         username: true,
-        email: false, // Optional
-        phone: false, // Optional
-      }
+        email: false
+      },
+      
     }
   }
 });
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
